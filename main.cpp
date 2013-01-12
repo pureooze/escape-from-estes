c566850ca9147512379e4201fa88da4c9aa16973
gum <gum@jsrf.(none)>
2013-01-12 03:35:07 -0500

    Started working

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+char gameStart();
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    gameStart();
     return 0;
+}
+
+char gameStart()
+{
+
+    char ans;
+
+    printf("Welcome to Escape from Estes!\n");
+    printf("[1] New Game\n[2] Load Game\n[3] Options\n[4] Exit\n\n");
+
+    cin >> ans;
+    return ans;
 }
